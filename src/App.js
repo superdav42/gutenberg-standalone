1cc120afc8e4ba04fde7c2abf2882fc5c8fb04fb
David Stone <david@nnucomputerwhiz.com>
2022-05-24 08:31:30 -0600

    initial try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.scss';
+
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+import {
+  BlockEditorKeyboardShortcuts,
+  BlockEditorProvider,
+  BlockList,
+  BlockTools,
+  BlockInspector,
+  WritingFlow,
+  ObserveTyping,
+} from '@wordpress/block-editor';
+import { Popover, SlotFillProvider } from '@wordpress/components';
+import { ShortcutProvider } from '@wordpress/keyboard-shortcuts';
+import '@wordpress/format-library';
 
 function App() {
+  const [ blocks, updateBlocks ] = useState( [] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="playground">
+        <ShortcutProvider>
+          <SlotFillProvider>
+            <BlockEditorProvider
+                value={ blocks }
+                onInput={ updateBlocks }
+                onChange={ updateBlocks }
+            >
+              <div className="playground__sidebar">
+                <BlockInspector />
+              </div>
+              <div className="playground__content">
+                <BlockTools>
+                  <div className="editor-styles-wrapper">
+                    <BlockEditorKeyboardShortcuts.Register />
+                    <WritingFlow>
+                      <ObserveTyping>
+                        <BlockList />
+                      </ObserveTyping>
+                    </WritingFlow>
+                  </div>
+                </BlockTools>
+              </div>
+              <Popover.Slot />
+            </BlockEditorProvider>
+          </SlotFillProvider>
+        </ShortcutProvider>
+      </div>
+  )
 }
+
 
 export default App;
